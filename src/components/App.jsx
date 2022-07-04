2b90280c545dca14f4fb7a7fa85c4680ebf99544
Shubham Agrahari <77830978+shubham12346@users.noreply.github.com>
2022-07-04 10:20:58 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Card from "./Card" 
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <Card 
+      name="";
+      src="";
+      info1 ="";
+      info2= ""; 
+      />
+    </div>
+  );
+}
+
+export default App;
